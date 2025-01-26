abb43f211823e00c9b175e7fd110a07a7c2ec8db
BisalDangol <message2bishal@outlook.com>
2025-01-26 15:47:37 +0545

    visitor with comment and to-do list functionality

--- models/visitors/admin.py
+++ models/visitors/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Visitors
+from .models import Visitors, Comment, TodoList
 
 admin.site.register(Visitors)
+admin.site.register(Comment)
+admin.site.register(TodoList)
--- models/visitors/models.py
+++ models/visitors/models.py
@@ -1,8 +1,27 @@
 from django.db import models
 from team.models import Teams
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+class Comment(models.Model):
+    context = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at= models.DateTimeField(auto_now=True)
+    created_by = models.ForeignKey(User, related_name='comments', on_delete=models.CASCADE)
+
 class Visitors(models.Model):
-    name : models.CharField(max_length=255) # type: ignore
-    teams: models.ForeignKey(Teams, related_name='clients', on_delete=models.CASCADE) # type: ignore
-#we can use SET_NULLinsted of CASCADE
+    name = models.CharField(max_length=255)
+    teams= models.ForeignKey(Teams, related_name='clients', on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at= models.DateTimeField(auto_now=True)
+#we can use SET_NULL insted of CASCADE
+
+
+
+class TodoList(models.Model):
+    Visitors= models.ForeignKey(Visitors,related_name='todoLists', on_delete=models.CASCADE)
+    name = models.CharField(max_length=255)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at= models.DateTimeField(auto_now=True)
+    created_by = models.ForeignKey(User, related_name='todoLists', on_delete=models.CASCADE)
